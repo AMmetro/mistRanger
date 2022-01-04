094a456e7f56d11165358ddc7cf03c01633b04c1
AMmetro <metro.trend.now@gmail.com>
2022-01-04 23:47:22 +0300

    add search in DB routes

--- routes/unit-routes.js
+++ routes/unit-routes.js
@@ -6,10 +6,6 @@ const createPath=require('../helpers/create-path')
 router.get('/', (req, res) => {
     res.sendFile(createPath('index'));
   });
-
-router.get('/', (req, res) => {
-    res.sendFile(createPath('index'));
-  });
   
   router.get('/contacts', (req, res) => {
     res.sendFile(createPath('contacts'));
@@ -19,6 +15,21 @@ router.get('/', (req, res) => {
     res.redirect('/contacts');
   });
   
+  router.get('/posts/', (req, res) => {
+    // console.log(req.url);
+    Unit
+       .find()
+       .sort({createdAt: -1})
+       .then((units)=> res.send(units))
+  });
+
+  // router.get('/find/:id', (req, res) => {
+  //   Unit
+  //   .find({title: req.params.id}, {})
+  //   // .then((res)=>{console.log(res)})
+  //   .then((unit)=>{res.send(unit)})
+  // });
+
   router.get('/posts/:id', (req, res) => {
     Unit
     .findById(req.params.id)
@@ -33,58 +44,27 @@ router.get('/', (req, res) => {
     res.send(error)
     });
   });
-  // --edit-------------
-  router.put('/edit/:id', (req, res) => {
-    // const {title, author, text} = req.body
-    const {id}=req.params
-      // ------------------------------------------------------------------------
-      const temp = { id: new Date(), date: (new Date()).toLocaleDateString(), text: "edit edit", title:"edit edit", date:"05.05.2021", author:"Andron"}
-      const {title, author, text} = temp
-      // console.log(title)
-      // console.log(author)
-      // console.log(text)
-      const unit = new Unit ({title, author, text})
-    // ----------------------------------------------------------------------------
+
+  router.put('/edit/', (req, res) => {
+    const {title, author, text, id} = req.body
     Unit
-    .findByIdAndUpdate(id, {title, author, text})
-    .then((result)=>{res.redirect(`/units/${id}`)})
-    // .then((result)=>{res.sendStatus(200)})
+    .findByIdAndUpdate(id, {title, author, text})  // третий передоваемый обект - вернуть обновленное значение
+    // .then((result)=>{res.redirect(`/units/${id}`)})
+    .then((result)=>{res.sendStatus(200)})
     .catch((error) => {console.log(error)
     res.send(error)
     });
   });
   
-  router.get('/posts/', (req, res) => {
-    // console.log(req.url);
-    Unit
-       .find()
-       .sort({createdAt: -1})
-       .then((units)=> res.send(units))
-    // const posts = [
-    //   {id:"1", text: "111111111", title:"post title", date:"05.05.2021", author:"Andron"},
-    //   {id:"2", text: "22222222", title:"post title", date:"05.05.2021", author:"Andron"},
-    //   {id:"3", text: "33333333", title:"post title", date:"05.05.2021", author:"Andron"},
-    // ]
-    // // res.sendFile(createPath('posts'));
-    //  res.send(posts);
-  });
-  
   router.post('/add-post/', (req, res) => {
-    // const {title, author, text} = req.body
-    // console.log(req.body)
-    
-    // ------------------------------------------------------------------------
-    const temp = { id: new Date(), date: (new Date()).toLocaleDateString(), text: "post post post", title:"new new", date:"05.05.2021", author:"Andron"}
-    const {title, author, text} = temp
-    // console.log(title)
-    // console.log(author)
-    // console.log(text)
-    const unit = new Unit ({title, author, text})
+     const {title, author, text} = req.body
+  console.log(title, author, text)
+  // ------------------------------------------------------------------------
+    // const temp = { id: new Date(), date: (new Date()).toLocaleDateString(), text: "post post post", title:"new new", date:"05.05.2021", author:"Andron"}
+    // const {title, author, text} = temp
   // ----------------------------------------------------------------------------
-      // console.log("req.body is =");
-      // console.log(req.body);
-      // res.send(req.body);
-      unit
+  const unit = new Unit ({title, author, text})
+    unit
       .save()
       .then(result=>res.send(req.body))
       .catch((error)=> {console.log(error)
